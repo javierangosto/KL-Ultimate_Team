be0cad9edf239d94f04d604a4c01d4d59470c26a
Javier Angosto <javier.angosto@gmail.com>
2023-04-15 19:35:21 +0200

    Inicio del proyecto KL Ultimate Team

---
+++ src/KLultimateTeamApp.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+export const KLultimateTeamApp = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>KLultimateTeamApp</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { KLultimateTeamApp } from './KLultimateTeamApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KLultimateTeamApp />
+  </React.StrictMode>,
+)
